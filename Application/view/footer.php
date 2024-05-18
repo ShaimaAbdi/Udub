2638074d67f701f68bedd9ad88d8edda163d35b4
shaimaXcoding <125131579+ShaimaAbdi@users.noreply.github.com>
2024-05-18 21:09:24 +0300

    Delete Application/view directory

--- Application/view/footer.php
+++
@@ -1,3 +0,0 @@
-<script src="../../assets/js/vendor-all.min.js"></script>
-	<script src="../../assets/plugins/bootstrap/js/bootstrap.min.js"></script>
-    <script src="../../assets/js/pcoded.min.js"></script>
